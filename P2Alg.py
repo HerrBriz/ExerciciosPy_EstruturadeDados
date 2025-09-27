321495a03b54aeefe730ab2a3febfd1dd71f7a67
Luiz Roberto Briz <luiz.quirino@fatec.sp.gov.br>
2025-09-27 13:34:19 -0300

    feat(#1 Main): Adicionando questões de 5 a 11.

--- P2Alg.py
+++ P2Alg.py
@@ -17,3 +17,49 @@ precos = "19.90 35.00 12.50 9.90".split()
 print ("Total: R$" + soma(precos))
 
 #4) Defina pares_finais(n). A função recebe um número inteiro positivo n. Ela deve retornar quantos dígitos pares aparecem seguidos no final do número. Exemplos: pares_finais(245680) -> 3, pares_finais(123456) -> 1, pares_finais(13) -> 0.
+
+
+#5) Enade 2011. No livro "O Homem que Calculava", de Malba Tahan, um personagem desejava ganhar os grãos de trigos que fossem distribuídos sobre um tabuleiro de xadrez do seguinte modo: um grão na primeira casa do tabuleiro, o dobro (2) na segunda, novamente o dobro (4) na terceira, outra vez o dobro (8) na quarta, e assim por diante, até a sexagésima quarta casa do tabuleiro.
+#Faça um algoritmo que calcule a quantidade total de grãos de trigo necessários para realizar esta distribuição. Não use o operador ** de exponenciação neste exercício.
+
+
+#6) Crie uma função mesmas_letras(s1, s2) que verifique se as strings s1 e s2 contêm exatamente as mesmas letras, independentemente da ordem e da quantidade.
+#Exemplos: mesmas_letras('banana', 'abn') -> True, mesmas_letras('casal', 'casal') -> True, mesmas_letras('casal', 'casa') -> False, mesmas_letras('amor', 'roma') -> True.
+#Apesar de ser possível de ser feito em uma única linha, você pode usar while ou for e fazer em várias linhas.
+
+
+#7) def maldição(s). A entrada é uma string s, com letras minúsculas e sem acentos, o retorno é uma nova string, onde a vogal que mais aparece deve ser trocada por '#'.
+#Se houver empate pegue a primeira vogal mais frequente. Use count() para contar quantas vezes cada vogal aparece. Você precisa necessariamente fazer alguma repetição entre todas as vogais, não vale as contagens em cinco variáveis.
+#Depois, use replace() para fazer a troca. Exemplos: maldição('abracadabra bobo sem nocao')->'#br#c#d#br# bobo sem noc#o', maldição('aaaxeeeeexxiiiiixxxooooxxxuuuu')->'##xeeeeexxiiiiixxxooooxxxuuu'
+
+
+#8) def sanduiche_com_vogais(s). Conta quantas vezes um padrão "sanduíche com vogais" aparece em uma string. Um sanduíche com vogais é uma sequência de 3 caracteres onde o primeiro e o terceiro são vogais e o segundo pode ser qualquer caractere.
+#Exemplos: sanduiche('amazing') -> 2, sanduiche('cooool') -> 2, sanduiche('aeiouAEIOU') -> 8, sanduiche('') -> 0
+
+
+#9) def soma_duplas(nums). Dada uma lista de inteiros nums, some cada número que aparece exatamente duas vezes, apenas uma vez na soma final.
+#Exemplos: soma_duplas([1, 2, 2, 5, 3, 3, 3, 4, 4]) -> 2+4=6, soma_duplas([1, 2, 1, 2, 3]) -> 1+2=3, soma_duplas([]) -> 0.
+
+
+#10) Mostre a resposta do código abaixo:
+x = ['', 'abacate', [], [42, 'resposta'], [], [42], 0, 42, 3 = 3.0, '' in 'abacate']
+res = []
+while x:
+    item = x[0]
+    x = x[1:]
+    if item:
+        res.append(item)
+    else:
+        res.append('Falso lógico')
+
+print(res)
+
+if [42] in res: print('Eu sou Feliz!')
+if res.count('Falso lógico') == 4: print(res.count(True))
+if res.count('Falso') == 0: print(res.count('Falso!'))
+if False: print('Falso!')
+if True: print('Vou aprender mais!')
+
+
+#11)  Bônus. Implemente a função espelho_frase(frase) que recebe uma string contendo uma frase formada por várias palavras separadas por espaço e devolve uma nova string onde a ordem das palavras é invertida e cada palavra é transformada em seu espelho, ou seja, seus caracteres aparecem na ordem inversa.
+#Não utilize estruturas de repetição como for ou while. Utilize apenas funções e métodos de string, como split(), join() e fatiamento com [::-1]. Exemplos: espelho_frase("Python é Legal") -> "lageL é nohtyP"
